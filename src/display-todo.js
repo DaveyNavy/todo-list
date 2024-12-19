b4ef02d5d64ada4d70221e63b1909f9ca46d369d
David Zhu <davidyzhu2@gmail.com>
2024-12-18 17:02:14 -0800

    Add progress circle to todo item

--- src/display-todo.js
+++ src/display-todo.js
@@ -1,13 +1,27 @@
+import CircleProgress from "js-circle-progress";
+
 export function displayToDo(todo) {
     const container = document.querySelector(".content");
 
     const div = document.createElement("div");
     div.classList.add("todo-item");
 
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("todo-item-title")
+
+    const cp = new CircleProgress({
+        max: 100,
+        value: 0,
+        textFormat: 'none',
+    });
+
     const title = document.createElement("h1");
     title.textContent = todo.getTitle();
     title.classList.add("todo-item-title");
-    div.appendChild(title);
+    
+    titleDiv.appendChild(cp);
+    titleDiv.appendChild(title);
+    div.appendChild(titleDiv);
 
     const dueDate = document.createElement("h3");
     dueDate.textContent = todo.getDueDate();
@@ -17,6 +31,15 @@ export function displayToDo(todo) {
     description.textContent = todo.getDescription();
     div.appendChild(description);
 
+    const tasks = document.createElement("h2");
+    tasks.textContent = "Tasks";
+    div.appendChild(tasks);
+
+    const hr = document.createElement("hr")
+    div.appendChild(hr);
+
+    let totalChecked = 0;
+    let total = 0;
     const form = document.createElement("form");
     todo.getChecklist().forEach((element, index) => {
         const checklistDiv = document.createElement("div");
@@ -29,6 +52,15 @@ export function displayToDo(todo) {
         const input = document.createElement("input");
         input.setAttribute("type", "checkbox");
         input.setAttribute("name", "item" + index);
+        input.addEventListener("change", function() {
+            if (this.checked) {
+                totalChecked++;
+            } else {
+                totalChecked--;
+            }
+            cp.value = totalChecked / total * 100;
+        })
+        total++;
 
         checklistDiv.appendChild(input);
         checklistDiv.appendChild(label);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles.css";
 import { ToDoItem } from "./todo-item.js";
 import { displayToDo } from "./display-todo.js";
 
-let myToDo = new ToDoItem("Wash Dishes", "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolore earum delectus dignissimos eveniet rerum aspernatur soluta dolor molestiae nemo similique iste voluptatibus quae vero ut, necessitatibus iure ex assumenda.", "12/17/24", "High", ['a', 'b', 'c']);
+let myToDo = new ToDoItem("Wash Dishes", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolore earum delectus dignissimos eveniet rerum aspernatur soluta dolor molestiae nemo similique iste voluptatibus quae vero ut, necessitatibus iure ex assumenda.", 
+    "12/17/24", "High", ['Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.']);
 
 
 displayToDo(myToDo);
